6a48d3c5619987ef11cc2cd2174b05066b6974f1
Um1djon2008 <163114146+Um1djon2008@users.noreply.github.com>
2024-03-16 15:11:38 +0500

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,8 @@
+// let lastName='Umidjon';
+// let firstName='Sharifqulov';
+//
+// let fullName=lastName+' '+firstName ;
+//
+// console.log(fullName)
+//
+// document.getElementById("d").innerHTML='Assalomu Alaykum! '+ fullName
